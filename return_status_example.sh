bdfd6e08176931c3649fee6ac99ee41a9ea9479f
Blaise Fringel <ffringel9@gmail.com>
2017-05-17 22:01:23 +0100

    Functions example

---
+++ return_status_example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Setting a return status for a function
+
+print_something () {
+    echo Hello $1
+    return 5
+}
+
+print_something Mars
+print_something Jupiter
+echo The previous function has a return value of $?
